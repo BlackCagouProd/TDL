ecea605e398ce7429271d8fe207e3e22021d5c27
BlackCagouProd <blackcagou@outlook.fr>
2022-09-06 00:22:03 +0200

    add files

---
+++ app.js
@@ -0,0 +1,21 @@
+let aToDo = document.getElementById('Add');
+let cTodo = document.getElementById('contain');
+let iToDo = document.getElementById('iField');
+
+
+
+function ajout() {
+    const todo = document.createElement('p');
+    todo.innerText = iToDo.value;
+    cTodo.appendChild(todo);
+    iToDo.value = "";
+    todo.addEventListener('click', function() {
+        todo.style.textDecoration = "line-through"
+    })
+    todo.addEventListener('dblclick', function() {
+        cTodo.removeChild(todo);
+    })
+
+}
+
+aToDo.addEventListener('click', ajout)
